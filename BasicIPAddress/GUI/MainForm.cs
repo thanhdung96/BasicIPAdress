dab7f4bb01960cc118bb1687fe72f8e2041c1dcd
thanhdung96 <14dh110224@st.huflit.edu.vn>
2017-09-15 12:32:57 +0700

    Init Project

---
+++ BasicIPAddress/GUI/MainForm.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace BasicIPAddress.GUI
+{
+	public partial class MainForm : Form
+	{
+		private IPAddress ipAddress;
+
+
+		public MainForm()
+		{
+			InitializeComponent();
+			ipAddress = IPAddress.Parse("192.168.1.150");
+
+		}
+	}
+}
